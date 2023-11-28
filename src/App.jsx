21003ba5e5c94f3d0eb7dde44ad03c158a22ca4e
Hưng Nguyễn <hungnguyen@Hungs-MacBook-Air.local>
2023-11-28 13:34:24 +0700

    add file modifile

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,9 @@
-import Button from "@mui/material/Button";
-import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
-import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
-import Typography from "@mui/material/Typography";
-
-import { useColorScheme } from "@mui/material/styles";
-
-// CSR, SSR
-
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme();
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === "light" ? "dark" : "light");
-        // localStorage.setItem('trello-dark-light-mode')
-        // localStorage.getItem('trello-dark-light-mode')
-      }}
-    >
-      {mode === "light" ? "Turn dark" : "Turn light"}
-    </Button>
-  );
-}
+import Board from "./pages/Boards/_id";
 
 function App() {
   return (
     <>
-      <ModeToggle />
-      <hr />
-      <div>Yushing Dev</div>
-      <Typography variant="body2" color="text.secondary">
-        Test Typography
-      </Typography>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-
-      <AccessAlarmIcon />
-      <ThreeDRotation />
+      <Board />
     </>
   );
 }
